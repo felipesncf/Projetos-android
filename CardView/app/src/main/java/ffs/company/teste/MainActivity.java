c1d5423ae8ef7b1584c433ccdce98861771d09b9
Felipe Neiva <felipec798@gmail.com>
2019-11-23 21:03:47 -0300

    AlunoSQLite e CardView e ReciclyView

---
+++ CardView/app/src/main/java/ffs/company/teste/MainActivity.java
@@ -0,0 +1,89 @@
+package ffs.company.teste;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private static RecyclerView recyclerView;
+    private static ArrayList<DataModel> data;
+    static View.OnClickListener myOnClickListener;
+    private static ArrayList<Integer> removedItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myOnClickListener = new MyOnClickListener(this);
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        data = new ArrayList<DataModel>();
+        for (int i = 0; i < ffs.company.teste.MyData.nameArray.length; i++) {
+            data.add(new DataModel(
+                    ffs.company.teste.MyData.nameArray[i],
+                    ffs.company.teste.MyData.versionArray[i],
+                    ffs.company.teste.MyData.id_[i],
+                    ffs.company.teste.MyData.drawableArray[i]
+            ));
+        }
+
+        removedItems = new ArrayList<Integer>();
+
+        adapter = new CustomAdapter(data);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    private static class MyOnClickListener implements View.OnClickListener {
+
+        private final Context context;
+
+        private MyOnClickListener(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onClick(View v) {
+            removeItem(v);
+        }
+
+        private void removeItem(View v) {
+            int selectedItemPosition = recyclerView.getChildPosition(v);
+            RecyclerView.ViewHolder viewHolder
+                    = recyclerView.findViewHolderForPosition(selectedItemPosition);
+            TextView textViewName
+                    = (TextView) viewHolder.itemView.findViewById(R.id.textViewName);
+            String selectedName = (String) textViewName.getText();
+            int selectedItemId = -1;
+            for (int i = 0; i < ffs.company.teste.MyData.nameArray.length; i++) {
+                if (selectedName.equals(ffs.company.teste.MyData.nameArray[i])) {
+                    selectedItemId = ffs.company.teste.MyData.id_[i];
+                }
+            }
+            removedItems.add(selectedItemId);
+            data.remove(selectedItemPosition);
+            adapter.notifyItemRemoved(selectedItemPosition);
+        }
+    }
+}
---
+++ CardView/app/src/main/java/ffs/company/teste/MyData.java
@@ -0,0 +1,16 @@
+package ffs.company.teste;
+
+
+import ffs.company.teste.R;
+
+public class MyData {
+
+    static String[] nameArray = {"Coração", "Click", "YouTube", "Like", "FaceBook", "Violão", "Setembro amarelo","Ideia", "Emoji", "Estrela", "Mulher Maravilha"};
+    static String[] versionArray = {" azul", "mouse", "vídeos", "facebook", "rede social", "brinquedo", "campanha", "brilhante", "feliz", "verde","filme"};
+
+    static Integer[] drawableArray = {R.drawable.a, R.drawable.a1, R.drawable.a2,
+            R.drawable.a3, R.drawable.a4, R.drawable.a5, R.drawable.a6,
+            R.drawable.a7, R.drawable.a8, R.drawable.a9,R.drawable.a10};
+
+    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+}
